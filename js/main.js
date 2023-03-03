a1006af9524d80f31b4fa5f1a3f29b894853df64
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-03 16:19:53 +0100

    aggiunta funzione click ai quadrati

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const griagliaDom = document.querySelector('#griglia');
 for (let i = 0; i < 100; i++) {
     const currentQuadrato = createQuadrato();
 
+    currentQuadrato.addEventListener('click', function() {
+        this.classList.toggle('cliccato')
+
+    })
+
     griagliaDom.append(currentQuadrato);
 
 }
